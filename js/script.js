c6e85f4df93eefcb23bc839fe1a17487ffbe925a
Shawinder Sekhon <shawinder@msn.com>
2015-10-01 15:07:58 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://formspree.io/shawinder@live.com",
                 type: "POST",
                 data: {
                     name: name,
